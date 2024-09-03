5dff28578e0bed44b636751b977d0227513f7775
Ranjith kumar <116632114+Ranjithkumar7760@users.noreply.github.com>
2024-09-03 12:51:46 +0530

    updated the files

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cde1916684e71f06387723f17494aad19842e7e75f16d8b7e16970e42de3f64
-size 2055
+oid sha256:d6da0f5a310b281469a9ef1a9d56e33ca6e064048e449209480ef44092cee2aa
+size 2899
